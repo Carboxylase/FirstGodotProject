99acc9bc39a321dddbf5962d6b87dfbba9073280
unknown <siddharth.naik301@gmail.com>
2026-02-23 21:15:01 -0500

    added texutres

--- Player.cs
+++ Player.cs
@@ -5,23 +5,32 @@ public partial class Player : CharacterBody3D
 {	
 	public float PixelsPerMeter = 100F;
 	
+	// Player Movement Settings
 	[Export]
-	public float groundSpeedMetersPerSecond = 5F;
+	public float groundSpeedMetersPerSecond = 8.5F;
+	[Export]
+	public float groundSpeedSprintMetersPerSecond = 12.5F;
 	public float groundSpeed;
 	
 	[Export]
-	public float jumpSpeedMetersPerSecond = 20F;
+	public float jumpSpeedMetersPerSecond = 4.5F;
 	public float jumpSpeed;
 	
 	[Export]
 	public float gravityMetersPerSecond = 9.8F;
 	public float gravity;
 
+	public Vector3 velocity = Vector3.Zero;
+
+	// Player Mouse Settings
 	[Export]
-	public float mouseRotationSpeed = (float)MathF.PI / 400;//0.1125F; // 90 degrees/800 pixels
+	public float mouseRotationSpeed = MathF.PI / 800;//0.1125F; // 90 degrees/800 pixels
 
 	public Vector2 prevMousePosition = Vector2.Zero;
 	
+	public Vector2 mouseMovementDelta = Vector2.Zero;
+	
+	// Player Camera Settings
 	public Vector3 prevCameraPosition = Vector3.Zero;
 	
 	[Export]
@@ -30,7 +39,9 @@ public partial class Player : CharacterBody3D
 	[Export]
 	public float cameraDistanceVertical = 1.5F;
 	
-	public Vector3 velocity = Vector3.Zero;
+	public float minVerticalCameraAngleRadians = -0.25F * MathF.PI;
+	
+	public float maxVerticalCameraAngleRadians = 0.60F * MathF.PI;
 	
 	public Vector3 prevPlayerRotation = Vector3.Zero;
 	
@@ -53,6 +64,9 @@ public partial class Player : CharacterBody3D
 		prevPlayerRotation = Rotation;
 		
 		playerLookDirection = new Vector3(1,0,0);
+		
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+		Input.UseAccumulatedInput = true;
 	}
 	
 	public override void _Ready()
@@ -69,6 +83,21 @@ public partial class Player : CharacterBody3D
 		prevMousePosition = GetViewport().GetMousePosition();
 	}
 	
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion mouseMovement)
+		{
+			mouseMovementDelta += mouseMovement.ScreenRelative;
+		}
+		if (@event is InputEventKey keyInput)
+		{
+			if (keyInput.Pressed && keyInput.Keycode == Key.Escape)
+			{
+				GetTree().Quit();
+			}
+		}	
+	}
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		applyGravity(delta);
@@ -80,9 +109,24 @@ public partial class Player : CharacterBody3D
 		MoveAndSlide();
 	}
 	
+	public override void _Process(double delta)
+	{
+		mouseMovementDelta = Vector2.Zero;
+	}
+	
 	private void getPlayerInput(double delta)
 	{	
 		//GD.Print($"X Component: {playerLookDirection3D.GlobalPosition.X - Position.X}, Z Component: {playerLookDirection3D.GlobalPosition.Z - Position.Z}");
+		
+		if (Input.IsActionPressed("Sprint"))
+		{
+			groundSpeed = groundSpeedSprintMetersPerSecond * PixelsPerMeter;
+		}
+		else
+		{
+			groundSpeed = 	groundSpeedMetersPerSecond * PixelsPerMeter;
+		}
+		
 		if (Input.IsActionPressed("Forward"))
 		{
 			velocity.X += (playerLookDirection3D.GlobalPosition.X - Position.X) * groundSpeed * (float)delta;
@@ -105,10 +149,10 @@ public partial class Player : CharacterBody3D
 		}
 		
 		Vector3 velocityBefore = velocity;
-		GD.Print("_________________________________");
-		GD.Print($"Player Look Direciton: {playerLookDirection3D.GlobalRotation.Y}");
-		GD.Print($"Velocity Before: {velocity}");
-		GD.Print($"Speed Before: {MathF.Sqrt(MathF.Pow(velocity.X, 2.0F) + MathF.Pow(velocity.Z, 2.0F))}");
+		//GD.Print("_________________________________");
+		//GD.Print($"Player Look Direciton: {playerLookDirection3D.GlobalRotation.Y}");
+		//GD.Print($"Velocity Before: {velocity}");
+		//GD.Print($"Speed Before: {MathF.Sqrt(MathF.Pow(velocity.X, 2.0F) + MathF.Pow(velocity.Z, 2.0F))}");
 		 //this is a bit problematic - while it does bring the velocity closer to the intended velocity, its fucking up somehow
 		if (velocity.X != 0 && velocity.Z != 0 )
 		{
@@ -116,7 +160,9 @@ public partial class Player : CharacterBody3D
 			velocity.X = velocity.X * normalizeCoefficient; 
 			velocity.Z = velocity.Z * normalizeCoefficient;
 		}
-		if (Input.IsActionPressed("Jump")) //&& IsOnFloor()
+		
+		//jump logic
+		if (Input.IsActionPressed("Jump") && IsOnFloor()) //&& IsOnFloor()
 		{
 			velocity.Y = jumpSpeed * (float)delta;
 		}
@@ -124,20 +170,25 @@ public partial class Player : CharacterBody3D
 		Velocity = velocity;
 		
 		Vector3 velocityAfter = velocity;
-		
+		GD.Print($"Velocity: {velocity}");
 		//GD.Print($"Velocity Ratio: {velocityBefore/velocityAfter}");
 		
-		GD.Print($"Look Direction Distance: {(float)Math.Sqrt((double)(playerLookDirection.X * playerLookDirection.X + playerLookDirection.Z * playerLookDirection.Z))}");
-		GD.Print($"Velocity After: {velocity}");
-		GD.Print($"Speed After: {MathF.Sqrt(MathF.Pow(velocity.X, 2.0F) + MathF.Pow(velocity.Z, 2.0F))}");
-		GD.Print("_________________________________");
-		velocity = Vector3.Zero;
+		//GD.Print($"Look Direction Distance: {(float)Math.Sqrt((double)(playerLookDirection.X * playerLookDirection.X + playerLookDirection.Z * playerLookDirection.Z))}");
+		//GD.Print($"Velocity After: {velocity}");
+		//GD.Print($"Speed After: {MathF.Sqrt(MathF.Pow(velocity.X, 2.0F) + MathF.Pow(velocity.Z, 2.0F))}");
+		//GD.Print("_________________________________");
+		float persistentVerticalVelocity = velocity.Y;
+		Vector3 resetVelocity = Vector3.Zero;
+		resetVelocity.Y = persistentVerticalVelocity;
+		velocity = resetVelocity;
+		
 	}
 	
 	private void updatePlayerPerspective()
 	{
 		Vector2 currentMousePosition = GetViewport().GetMousePosition();
-		Vector2 mouseDelta = currentMousePosition - prevMousePosition;
+		//Vector2 mouseDelta = currentMousePosition - prevMousePosition;
+		Vector2 mouseDelta = mouseMovementDelta;
 		prevMousePosition = currentMousePosition;
 		
 		// rotate the entire Player instance
@@ -151,14 +202,36 @@ public partial class Player : CharacterBody3D
 		Vector3 cameraMarkerRotationRadians = cameraMarker.Rotation;
 		float mouseAngleDeltaVertical = -mouseDelta.Y * mouseRotationSpeed;
 		cameraMarkerRotationRadians.Z += mouseAngleDeltaVertical;
-		cameraMarker.Rotation = cameraMarkerRotationRadians;
 		
+		// moving the mouse down makes the cameraMarkerRotation increase
+		float cameraRotationPlayerRelative = camera.Rotation.Z + cameraMarker.Rotation.Z;
+		
+		//looking down
+		if (cameraRotationPlayerRelative <= minVerticalCameraAngleRadians && mouseDelta.Y > 1)
+		{
+			//cameraMarkerRotationRadians.Z = minVerticalCameraAngleRadians - camera.Rotation.Z;
+			return;
+		}
+		
+		//looking up
+		if (cameraRotationPlayerRelative >= maxVerticalCameraAngleRadians && mouseDelta.Y < 0)
+		{
+			//cameraMarkerRotationRadians.Z = maxVerticalCameraAngleRadians - camera.Rotation.Z;
+			return;
+		}
+		//GD.Print($"Mouse Delta: {mouseDelta.Y}");
+		//GD.Print($"Rotation: {cameraMarker.Rotation}");
+		cameraMarker.Rotation = cameraMarkerRotationRadians;
+		return;
 	}
 	
 	private void applyGravity(double delta)
 	{
-		velocity.Y -= gravity * (float)delta;
+		velocity.Y -= gravity * (float)delta * (float)delta;
+		if (IsOnFloor())
+		{
+			velocity.Y = 0F;
+		}
 	}
-	
 	
 }
